62de1015865d6f211b123f3dd281d8db640c31d3
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-01-21 13:17:18 +0600

    added js code and little css style

--- js/dom.js
+++ js/dom.js
@@ -1,18 +1,27 @@
-// const title=document.getElementsByTagName('h1');
-// console.log(title)
-
+// getElementsByTagName
+const title=document.getElementsByTagName('h1');
+console.log(title);
 const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
 for(const li of liCollection){
     console.log(li.innerText);
 }
+
+// getEelemntById
 const basicProjectList = document.getElementById('basic-procets');
-// console.log(basicProjectList);
+console.log(basicProjectList);
 
-// console.log(document.getElementsByClassName('Advanced-project'));
-
+// getElementsByClassName
+console.log(document.getElementsByClassName('Advanced-project'));
 const AdvancedProjects = document.getElementsByClassName('Advanced-project');
-// console.log(AdvancedProjects);
 for(const li of AdvancedProjects){
     console.log(li.innerText);
 }
+
+// querySelectorAll
+const intermidateProjects = document.querySelectorAll('.intermidate-projects li');
+console.log(intermidateProjects);
+
+// querySelector
+const intermidateProject = document.querySelector('.intermidate-projects li');
+console.log(intermidateProject);
