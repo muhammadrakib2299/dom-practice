e32acabc7c90d74d54f30ce5e31849edee562e15
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-01-21 12:43:35 +0600

    added html content, add litle css and edit some js code

--- js/dom.js
+++ js/dom.js
@@ -3,10 +3,16 @@
 
 const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
-
 for(const li of liCollection){
     console.log(li.innerText);
 }
+const basicProjectList = document.getElementById('basic-procets');
+// console.log(basicProjectList);
 
+// console.log(document.getElementsByClassName('Advanced-project'));
 
-
+const AdvancedProjects = document.getElementsByClassName('Advanced-project');
+// console.log(AdvancedProjects);
+for(const li of AdvancedProjects){
+    console.log(li.innerText);
+}
