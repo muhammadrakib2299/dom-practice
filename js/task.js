88532f1bb8d15cae81012a330c7597a9a4a7532b
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-01-26 23:46:40 +0600

    daily task simple project completed

--- js/task.js
+++ js/task.js
@@ -1,3 +1,4 @@
+// Create the daily task and display the task
 //select the button
 document.getElementById('update-btn').addEventListener('click', function(){
     //select the input filed
@@ -8,10 +9,13 @@ document.getElementById('update-btn').addEventListener('click', function(){
     const taskContainer = document.getElementById('task-container');
     const createTaskElementLI = document.createElement('li');
     createTaskElementLI.innerText = taskContent;
-    // console.log(createTaskElementLI);
+    createTaskElementLI.classList.add('item');
     // append the create element item into the task container
     taskContainer.appendChild(createTaskElementLI);
     taskField.value = ' ';
-    
-
 });
+
+// delete the daily created task
+document.getElementById('task-container').addEventListener('click', function(event){
+    event.target.parentNode.removeChild(event.target);
+})
